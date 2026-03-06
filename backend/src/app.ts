f8ccba67b4f24e9b4789858cd491073d562b8c52
JROJAS-sys <justin.rojas.jarquin@est.una.ac.cr>
2026-03-06 10:59:22 -0600

    Hola

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/endpoint", (req, res) => {
+app.get("/ejemplo", (req, res) => {
   res.json({
     status: "Correcto",
     message: "Ejecutando en TypeScript",
